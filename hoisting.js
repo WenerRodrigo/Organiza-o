c48a3ddd9f974cb00fa2b8ceadfd38860dea236e
Wener Rodrigo <106413124+WenerRodrigo@users.noreply.github.com>
2022-06-08 21:42:28 -0300

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -5,6 +5,6 @@ const Cliente = function(){}
 console.log(typeof Cliente)
 console.log(typeof new Cliente)
 
-class Produto {} // ES 2015 (ES6)
+class Produto {} 
 console.log(typeof Produto)
 console.log(typeof new Produto)
